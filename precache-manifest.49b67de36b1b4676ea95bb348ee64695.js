e19aa106de34c2440edc58927f90a93305570164
saitejam12 <saitejamuddana@gmail.com>
2019-11-05 18:25:23 -0600

    Updates

---
+++ precache-manifest.49b67de36b1b4676ea95bb348ee64695.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e8bc3766e023f10673fc92672e7ea1",
+    "url": "/howdoiplay/index.html"
+  },
+  {
+    "revision": "a419258d1c2572d8545a",
+    "url": "/howdoiplay/static/css/main.52423e21.chunk.css"
+  },
+  {
+    "revision": "2e5aef4e20375a98d5a1",
+    "url": "/howdoiplay/static/js/2.5f71e873.chunk.js"
+  },
+  {
+    "revision": "a419258d1c2572d8545a",
+    "url": "/howdoiplay/static/js/main.619345a4.chunk.js"
+  },
+  {
+    "revision": "e6484284504c086d97ed",
+    "url": "/howdoiplay/static/js/runtime~main.027b03a8.js"
+  },
+  {
+    "revision": "21f887b81bf5d835e7143afdb72c69a6",
+    "url": "/howdoiplay/static/media/fontello.21f887b8.woff2"
+  },
+  {
+    "revision": "305533a7e8fe7ada1a677dd89f7e3a86",
+    "url": "/howdoiplay/static/media/fontello.305533a7.eot"
+  },
+  {
+    "revision": "3a78fa362d401dd833e2085926da8bef",
+    "url": "/howdoiplay/static/media/fontello.3a78fa36.woff"
+  },
+  {
+    "revision": "f329234a1617a75b6c6cacd24e975fe7",
+    "url": "/howdoiplay/static/media/fontello.f329234a.ttf"
+  },
+  {
+    "revision": "f58c1e4c54d9722458db50bdfa4c43d8",
+    "url": "/howdoiplay/static/media/spritesheet.f58c1e4c.png"
+  }
+]);
